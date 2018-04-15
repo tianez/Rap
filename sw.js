b62aabaa6f5980f767973b19f16cf690657f856e
tianez <saber_tz@163.com>
2018-04-15 15:38:38 +0800

    1

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-var cacheStorageKey = "cacheStorage-v3";
+var cacheStorageKey = "cacheStorage-v4";
 
 var cacheList = [
     "/",
-    "index.html",
+    // "index.html",
     "manifest.json",
     "dll/vendor.dll.js",
     "public/bower/react/umd/react.production.min.js",
