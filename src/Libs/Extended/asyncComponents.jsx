3c015e77b30216f30dc5d5e4bf19d5865c8361b1
tianez <saber_tz@163.com>
2018-05-10 11:29:52 +0800

    111

--- src/Libs/Extended/asyncComponents.jsx
+++ src/Libs/Extended/asyncComponents.jsx
@@ -1,28 +1,54 @@
-import Loadable from 'react-loadable';
-import styles from './asyncComponents.scss';
+import React, { Component } from "react";
+import Loadable from "react-loadable";
+import { NavBar, Icon } from "antd-mobile";
+import Layout from "Components/Layout/Layout";
+import Content from "Components/Layout/Content";
+import LeftIcon from "Components/Layout/LeftIcon";
 
-const LoadingComponent = ({ isLoading, error }) => {
-    if (isLoading) {
+import styles from "./asyncComponents.scss";
+
+const LoadingFarmeWork = props => (
+    <Layout>
+        <NavBar mode="light" icon={<LeftIcon />} />
+        <Content>
+            <LoadingComponent {...props} />
+        </Content>
+    </Layout>
+);
+
+const LoadingComponent = ({ error, timedOut, pastDelay, retry, ...rest }) => {
+    console.log(rest);
+    if (error) {
+        return <LoadingError onClick={retry} title={"模块加载失败，点击重试"} />;
+    } else if (timedOut) {
+        return <LoadingError onClick={retry} title={"模块加载超时，点击重试"} />;
+    } else if (pastDelay) {
         return <Loading />;
-    } else if (error) {
-        return <LoadingError />;
-    } else {
-        return null;
     }
+    return null;
 };
 
-const LoadingError = () => (
-    <div>Sorry,页面模块加载失败</div>
+const LoadingError = ({ onClick, title }) => (
+    <div className={styles.loading}>
+        <div className={styles.loadingTitle} onClick={onClick}>
+            {title}
+        </div>
+    </div>
 );
 
 const Loading = () => (
-    <div>模块加载中...</div>
-)
+    <div className={styles.loading}>
+        <Icon type="loading" />
+        <div className={styles.loadingTitle}>模块加载中...</div>
+    </div>
+);
 
-const asyncComponent = importComponent => Loadable({
-    loader: () => importComponent(),
-    loading: LoadingComponent,
-    delay: 300
-})
+const asyncComponent = (importComponent, isRouter = false) =>
+    Loadable({
+        loader: () => importComponent(),
+        loading: props => (isRouter ? <LoadingFarmeWork {...props} /> : <LoadingComponent {...props} />),
+        delay: 200,
+        timeout: 6000
+    });
 
-export default asyncComponent
+export default asyncComponent;
