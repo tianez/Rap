d90f820cff093cb3d9f3788d1b16649a02d63e83
tianez <saber_tz@163.com>
2018-05-23 15:38:57 +0800

    111

--- builderh5.js
+++ builderh5.js
@@ -2,28 +2,29 @@ function plusReady() {
     // 隐藏滚动条
     plus.webview.currentWebview().setStyle({ scrollIndicator: "none" });
     // Android处理返回键
-    plus.key.addEventListener(
-        "backbutton",
-        function() {
-            "iOS" == plus.os.name
-                ? plus.nativeUI.confirm(
-                      "确认退出？",
-                      function(e) {
-                          if (e.index > 0) {
-                              plus.runtime.quit();
-                          }
-                      },
-                      "HelloH5",
-                      ["取消", "确定"]
-                  )
-                : confirm("确认退出？") && plus.runtime.quit();
-        },
-        false
-    );
-    compatibleAdjust();
+    plus.key.addEventListener("backbutton", BackButton, false);
 }
 if (window.plus) {
     plusReady();
 } else {
     document.addEventListener("plusready", plusReady, false);
+}
+
+function BackButton() {
+    if (window.location.hash != "#/home") {
+        return window.history.back();
+    }
+    if (plus.os.name != "iOS") {
+        return confirm("确认退出？") && plus.runtime.quit();
+    }
+    plus.nativeUI.confirm(
+        "确认退出？",
+        function(e) {
+            if (e.index > 0) {
+                plus.runtime.quit();
+            }
+        },
+        "HelloH5",
+        ["取消", "确定"]
+    );
 }
